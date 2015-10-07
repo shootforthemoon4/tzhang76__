567e175bf7a5beb1cd38b2d7fa2c36c9d64a9366
shootforthemoon4 <tinglizhangshootforthemoon@wirelessprvnat-172-16-184-35.near.illinois.edu>
2015-10-06 23:56:58 -0500

    Initial Commit

---
+++ tzhang76__/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tzhang76__
+//
+//  Created by Tingli Zhang on 10/6/15.
+//  Copyright © 2015 tzhang76. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
